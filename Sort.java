f18de2cbfd48cb55d7890f4091cbdc05acac2be1
Antoine Montier <115585369+antoineMontier@users.noreply.github.com>
2023-01-03 21:38:18 +0100

    added count

--- Sort.java
+++ Sort.java
@@ -91,5 +91,15 @@ public class Sort{
         }
         return -1;
     }
+
+    public static int count(int[] array, int value) {
+        int count = 0;
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] == value){
+                count++;
+            }
+        }
+        return count;
+    }
     
 }
