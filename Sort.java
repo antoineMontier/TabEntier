c1874a9d4fd91a250909c3c2462fe7328a373325
Antoine Montier <115585369+antoineMontier@users.noreply.github.com>
2023-01-03 21:19:22 +0100

    added swap maxIndex and selection sort

--- Sort.java
+++ Sort.java
@@ -1,17 +1,20 @@
 public class Sort{
     public static  void main(String[] args){
-        System.out.println("hey");
+        int[] k = {2, 1, 5, -8, 9};
+        selectionSort(k);
+        for(int i=0; i<k.length; i++){
+            System.out.printf("%d ", k[i]);
+        }
+        System.out.printf("\n");
+        System.out.println("max = " + maxIndex(k, k.length));
     }
 
     public static void bubbleSort(int[] array) {
-        int temp;
         for (int i = 0; i < array.length - 1; i++) {
             boolean sorted = true;
             for (int j = 0; j < array.length - i - 1; j++) {
                 if (array[j] > array[j + 1]) {
-                    temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
+                    swap(array, j, j + 1);
                     sorted = false;
                 }
             }
@@ -21,14 +24,33 @@ public class Sort{
         }
     }
 
-    public static int maxIndex(int[] array){
-        int index = 0;
-        for(int i = 0 ; i < array.length; i++){
-            if(array[index] < array[i]){
-                index = i;
+    public static void selectionSort(int[] array) {
+        int sortedSize = array.length - 1, index;
+        while (sortedSize >= 0) {
+            index = maxIndex(array, sortedSize);
+            if (index != sortedSize) {
+                swap(array, index, sortedSize);
+            }
+            sortedSize--;
+        }
+    }
+    
+    private static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    
+    private static int maxIndex(int[] array, int size) {
+        int maxIndex = 0;
+        for (int i = 1; i <= size; i++) {
+            if(i < array.length && array[i] > array[maxIndex]) {
+                maxIndex = i;
             }
         }
-        return index;
+        return maxIndex;
     }
+    
+    
     
 }
