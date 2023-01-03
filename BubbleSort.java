8e2882d3ef002857c4b9126aa6762a12816342c3
Antoine Montier <115585369+antoineMontier@users.noreply.github.com>
2023-01-03 20:44:01 +0100

    Create BubbleSort.java

---
+++ BubbleSort.java
@@ -0,0 +1,24 @@
+public class BubbleSort{
+    public static  void main(String[] args){
+        System.out.println("hey");
+    }
+
+    public static void bubbleSort(int[] array) {
+        int temp;
+        for (int i = 0; i < array.length - 1; i++) {
+            boolean sorted = true;
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    sorted = false;
+                }
+            }
+            if (sorted) {
+                break;
+            }
+        }
+    }
+    
+}
